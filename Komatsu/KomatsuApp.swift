60ad80c8cdd55e109a0acf61a4acf14ade445242
Seen <chatphongkeawkaw@gmail.com>
2026-05-06 11:41:29 +0700

    Initial Commit

---
+++ Komatsu/KomatsuApp.swift
@@ -0,0 +1,17 @@
+//
+//  KomatsuApp.swift
+//  Komatsu
+//
+//  Created by Sinnree on 6/5/2569 BE.
+//
+
+import SwiftUI
+
+@main
+struct KomatsuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
